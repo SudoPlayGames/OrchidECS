549b3cab21eed8dc40a1b1e1d6bc86aeb0f51be3
Jason Taylor <jason@codetaylor.com>
2018-06-02 11:08:15 -0700

    Add convenience constructors to object pool

--- src/main/java/com/sudoplay/ecs/core/ObjectPool.java
+++ src/main/java/com/sudoplay/ecs/core/ObjectPool.java
@@ -2,12 +2,23 @@ package com.sudoplay.ecs.core;
 
 import com.sudoplay.ecs.integration.api.Poolable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ObjectPool<P extends Poolable> {
 
-  private List<P> pool;
-  private Factory<P> factory;
+  private final List<P> pool;
+  private final Factory<P> factory;
+
+  public ObjectPool(Class<P> poolableClass) {
+
+    this(poolableClass, new ArrayList<P>());
+  }
+
+  public ObjectPool(Class<P> poolableClass, List<P> pool) {
+
+    this(ObjectPool.factory(poolableClass), pool);
+  }
 
   public ObjectPool(Factory<P> factory, List<P> pool) {
 
@@ -31,6 +42,26 @@ public class ObjectPool<P extends Poolable> {
 
     poolable.reset();
     this.pool.add(poolable);
+  }
+
+  public static <P extends Poolable> Factory<P> factory(final Class<P> poolableClass) {
+
+    return new Factory<P>() {
+
+      @Override
+      public P create() {
+
+        try {
+          return poolableClass.newInstance();
+
+        } catch (InstantiationException e) {
+          throw new RuntimeException("", e);
+
+        } catch (IllegalAccessException e) {
+          throw new RuntimeException("", e);
+        }
+      }
+    };
   }
 
   public interface Factory<T> {
